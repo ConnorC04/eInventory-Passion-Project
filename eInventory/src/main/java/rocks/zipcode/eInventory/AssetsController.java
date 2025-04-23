df87498626770910d5e8b6fa90cf07782b4b9c69
ConnorC04 <connorjcasey@iCloud.com>
2025-04-23 16:30:47 -0400

    im pretending its 5 o clock

--- eInventory/src/main/java/rocks/zipcode/eInventory/AssetsController.java
+++ eInventory/src/main/java/rocks/zipcode/eInventory/AssetsController.java
@@ -1,56 +1,20 @@
 package rocks.zipcode.eInventory;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import rocks.zipcode.eInventory.repositories.AssetsRepository;
 
-import jakarta.validation.Valid;
-
+import java.util.List;
 
 @RestController
-@RequestMapping("/authors")
-public class AssetsController {
+@RequestMapping("/assets")
+public class AssetsController{
+
+    private final AssetsRepository assetsRepository;
 
     @Autowired
-    private final Assets asset;
-
-    public AssetsController(Assets asset) {
-        this.asset = asset;
-    }
-
-    @GetMapping
-    public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
-        return ResponseEntity.ok(authorService.getAllAuthors());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<AuthorDTO> getAuthorById(@PathVariable Long id) {
-        return ResponseEntity.ok(authorService.getAuthorById(id));
-    }
-
-    @PostMapping
-    public ResponseEntity<AuthorDTO> createAuthor(@Valid @RequestBody AuthorDTO authorDTO) {
-        return new ResponseEntity<>(authorService.createAuthor(authorDTO), HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<AuthorDTO> updateAuthor(@PathVariable Long id, @Valid @RequestBody AuthorDTO authorDTO) {
-        return ResponseEntity.ok(authorService.updateAuthor(id, authorDTO));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
-        authorService.deleteAuthor(id);
-        return ResponseEntity.noContent().build();
+    public AssetsController(AssetsRepository assetsRepository){
+        this.assetsRepository = assetsRepository;
     }
 }
