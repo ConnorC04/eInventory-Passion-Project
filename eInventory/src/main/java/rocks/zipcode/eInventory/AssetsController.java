eb4563cf998d27d0323a6747f9db6b997af8f787
ConnorC04 <connorjcasey@iCloud.com>
2025-04-24 16:10:19 -0400

    controllers?

--- eInventory/src/main/java/rocks/zipcode/eInventory/AssetsController.java
+++ eInventory/src/main/java/rocks/zipcode/eInventory/AssetsController.java
@@ -1,19 +1,46 @@
 package rocks.zipcode.eInventory;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import java.util.Map;
 
 
 @RestController
 public class AssetsController{
 
-    private Assets assets = new Assets("helmet", ageClassification.JUNIOR, Sizes.SMALL);
+    private Map<String, Assets> db = new HashMap<>(){{
+        put("helmet", new Assets("helmet", ageClassification.SENIOR, Sizes.MEDIUM));
+    }};
+
+
 
     @GetMapping("/")
     public String index(){
         return "Hello World";
     }
 
+    @GetMapping("/assets")
+    public Collection<Assets> get(){
+        return db.values();
+    }
 
+    @GetMapping("/assets/{equipmentType}")
+    public Assets get(@PathVariable String equipmentType){
+        Assets assets = db.get(equipmentType);
+        if (assets == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return assets;
+    }
+
+    @DeleteMapping("/assets/{equipmentType}")
+    public void delete(@PathVariable String equipmentType){
+        Assets assets = db.remove(equipmentType);
+        if (assets == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
 
 }
