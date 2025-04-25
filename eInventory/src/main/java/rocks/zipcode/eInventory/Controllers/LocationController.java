9a53d1a639434d30097206d8cd56b998508884e2
ConnorC04 <connorjcasey@iCloud.com>
2025-04-25 15:08:03 -0400

    locations getting built up

--- eInventory/src/main/java/rocks/zipcode/eInventory/Controllers/LocationController.java
+++ eInventory/src/main/java/rocks/zipcode/eInventory/Controllers/LocationController.java
@@ -1,4 +1,30 @@
 package rocks.zipcode.eInventory.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import rocks.zipcode.eInventory.Classes.Locations;
+import rocks.zipcode.eInventory.Repositories.LocationsRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/locations")
+@CrossOrigin
 public class LocationController {
+
+    @Autowired
+    private LocationsRepository locationRepository;
+
+    @GetMapping
+    public List<Locations> getAllLocations() {
+        List<Locations> locations = new ArrayList<>();
+        locationRepository.findAll().forEach(locations::add);
+        return locations;
+    }
+
+    @PostMapping
+    public Locations addLocation(@RequestBody Locations location) {
+        return locationRepository.save(location);
+    }
 }
